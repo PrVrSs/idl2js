99f13171419094642be8f5bbb98b953f7a91d973
Sergey Reshetnikov <resh.sersh@gmail.com>
2023-06-16 22:56:44 +0700

    refactor

--- idl2js/fuzzer.py
+++ idl2js/fuzzer.py
@@ -5,7 +5,7 @@ class Fuzzer:
     def __init__(self, idls):
         self._transpiler = Transpiler(idls=idls)
 
-    def samples(self, idl_type, count = 3, options = None):
+    def samples(self, idl_type, count=3, options=None):
         cdg = self._transpiler.build_cdg(idl_type, options)
 
         for _ in range(count):
